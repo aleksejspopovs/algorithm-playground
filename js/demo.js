42e053453282be6e76928708c3676ae5466e3651
Aleksejs Popovs <aleksejs@popovs.lv>
2019-10-14 13:11:39 -0400

    Implement Spinner and Hold object, extend demo

--- js/demo.js
+++ js/demo.js
@@ -2,6 +2,52 @@ import {APG} from './apg.js'
 import {APGObject} from './object.js'
 import {SegmentTree} from './segmenttree.js'
 
+class Spinner extends APGObject {
+	constructor () {
+		super()
+		this.newOutputPlug('value')
+		this.state = {value: 0}
+	}
+
+	render (node) {
+		if (node.getElementsByTagName('input').length === 0) {
+			let new_input = document.createElement('input')
+			new_input.type = 'number'
+			new_input.addEventListener('input', (e) => {
+				let value = parseInt(e.target.value)
+				this.scheduleProcessing(() => {
+					this.state.value = value
+					this.output.value.write(value)
+				})
+			})
+			node.appendChild(new_input)
+		}
+
+		node.getElementsByTagName('input')[0].value = this.state.value
+	}
+}
+
+class Hold extends APGObject {
+	constructor () {
+		super()
+		this.newInputPlug('held')
+		this.newOutputPlug('value')
+	}
+
+	render (node) {
+		if (node.getElementsByTagName('button').length === 0) {
+			let button = document.createElement('button')
+			button.innerText = 'push'
+			button.addEventListener('click', (e) => {
+				this.scheduleProcessing(() => {
+					this.output.value.write(this.input.held.read())
+				})
+			})
+			node.appendChild(button)
+		}
+	}
+}
+
 class STVisualizer extends APGObject {
 	constructor () {
 		super()
@@ -46,7 +92,7 @@ class STInitializer extends APGObject {
 	}
 
 	ping () {
-		this.scheduleProcessing(() => this.output.tree.write(new SegmentTree(1, 10)))
+		this.scheduleProcessing(() => this.output.tree.write(new SegmentTree(0, 7)))
 	}
 }
 
@@ -54,14 +100,20 @@ let apg = new APG(document.body)
 apg._program.addObject(new STInitializer(), 'initializer')
 apg._program.addObject(new STVisualizer(), 'visualizer')
 apg._program.addObject(new STUpdater(), 'updater')
+apg._program.addObject(new Spinner(), 'index')
+apg._program.addObject(new Spinner(), 'value')
+apg._program.addObject(new Hold(), 'hold')
 
 apg._program.addWire('initializer', 'tree', 'updater', 'tree')
-// apg._program.addWire('updater', 'tree', 'updater', 'tree')
+apg._program.addWire('hold', 'value', 'updater', 'tree')
+apg._program.addWire('index', 'value', 'updater', 'index')
+apg._program.addWire('value', 'value', 'updater', 'value')
 apg._program.addWire('updater', 'tree', 'visualizer', 'tree')
+apg._program.addWire('updater', 'tree', 'hold', 'held')
 
 apg._program._objects['initializer'].ping()
 
-apg._program.schedulePlugUpdate('updater', 'index', 3)
-apg._program.schedulePlugUpdate('updater', 'value', 5)
-apg._program.schedulePlugUpdate('updater', 'index', 2)
-apg._program.schedulePlugUpdate('updater', 'value', 1)
+// apg._program.schedulePlugUpdate('updater', 'index', 3)
+// apg._program.schedulePlugUpdate('updater', 'value', 5)
+// apg._program.schedulePlugUpdate('updater', 'index', 2)
+// apg._program.schedulePlugUpdate('updater', 'value', 1)
