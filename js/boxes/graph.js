6bacc7561ab802ef30d561e63a5a920392d3192e
Aleksejs Popovs <aleksejs@popovs.lv>
2020-01-31 21:50:51 -0500

    Improve Graph box zooming behavior
    - Node markers and edge widths are no longer scaled.
    - There's now a "fit graph" button.

--- js/boxes/graph.js
+++ js/boxes/graph.js
@@ -41,16 +41,23 @@ export class Graph extends APGBox {
   }
 
   createLayout () {
+    let div = document.createElement('div')
+
     let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    div.appendChild(svg)
     svg.setAttribute('width', 320)
     svg.setAttribute('height', 240)
     svg.classList.add('Graph-svg')
     svg.style.border = '1px solid black'
+    svg.style.display = 'block'
 
     let d3svg = d3.select(svg)
-    d3svg.call(d3.zoom().on('zoom', () => {
-      d3svg.selectAll('.Graph-graph').attr('transform', d3.event.transform)
-    }))
+    let zoom = d3.zoom()
+    d3svg.call(zoom)
+
+    zoom.on('zoom', () => {
+      this.render(div)
+    })
 
     d3svg.append('circle')
         .attr('r', 4.5)
@@ -69,7 +76,27 @@ export class Graph extends APGBox {
         .attr('orient', 'auto')
       .append('path')
         .attr('d', 'M1,0.2 L1,0.8 L4,0.5 z')
-    return svg
+
+    let button = document.createElement('button')
+    div.appendChild(button)
+    button.innerText = 'fit graph'
+    button.addEventListener('click', () => this.scheduleProcessing(yieldControl => {
+      let boundingBox = this.state.graph.boundingBox()
+      zoom.translateTo(
+        d3svg,
+        (boundingBox.x1 + boundingBox.x2) / 2,
+        (boundingBox.y1 + boundingBox.y2) / 2,
+      )
+      let width = parseInt(svg.getAttribute('width'))
+      let height = parseInt(svg.getAttribute('height'))
+      let newScale = Math.min(
+        width / (boundingBox.x2 - boundingBox.x1 + 10),
+        height / (boundingBox.y2 - boundingBox.y1 + 10),
+      )
+      zoom.scaleTo(d3svg, newScale)
+    }))
+
+    return div
   }
 
   render (node) {
@@ -96,11 +123,10 @@ export class Graph extends APGBox {
       .join('line')
         .classed('Graph-graph', true)
         .classed('Graph-edge', true)
-        .attr('x1', (d) => d.from.x)
-        .attr('y1', (d) => d.from.y)
-        .attr('x2', (d) => d.to.x)
-        .attr('y2', (d) => d.to.y)
-        .attr('transform', zoom)
+        .attr('x1', (d) => zoom.applyX(d.from.x))
+        .attr('y1', (d) => zoom.applyY(d.from.y))
+        .attr('x2', (d) => zoom.applyX(d.to.x))
+        .attr('y2', (d) => zoom.applyY(d.to.y))
         .attr('marker-end', this.state.graph.directed ? 'url(#arrow-end)' : null)
         .on('click', (d) => {
           if (d3.event.altKey) {
@@ -119,9 +145,8 @@ export class Graph extends APGBox {
         .classed('Graph-graph', true)
         .classed('Graph-node', true)
         .classed('Graph-moving', false)
-        .attr('cx', (d) => d.x)
-        .attr('cy', (d) => d.y)
-        .attr('transform', zoom)
+        .attr('cx', (d) => zoom.applyX(d.x))
+        .attr('cy', (d) => zoom.applyY(d.y))
         .raise()
         .on('click', (d) => {
           if (d3.event.altKey) {
@@ -155,8 +180,8 @@ export class Graph extends APGBox {
             let node = d3.select(d3.event.sourceEvent.target)
             node.classed('Graph-moving', true)
             ghost.style('visibility', 'visible')
-                .attr('cx', d.x)
-                .attr('cy', d.y)
+                .attr('cx', zoom.applyX(d.x))
+                .attr('cy', zoom.applyY(d.y))
           })
           .on('drag', (d) => {
             // d3-drag's fancy coordinate computations (in d3.event.{x,y})
@@ -165,7 +190,7 @@ export class Graph extends APGBox {
               d3.event.sourceEvent.offsetX,
               d3.event.sourceEvent.offsetY,
             ])
-            ghost.attr('cx', x).attr('cy', y)
+            ghost.attr('cx', zoom.applyX(x)).attr('cy', zoom.applyY(y))
           })
           .on('end', (d) => {
             let [x, y] = d3.zoomTransform(svg.node()).invert([
